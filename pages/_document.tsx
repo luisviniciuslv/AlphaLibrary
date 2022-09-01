d14fa4c9e4acf0d88950d67f93ff9b799310d1b1
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2022-09-01 15:45:43 -0300

    Arrumando Meta Dados do projeto

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+
+          <title>AlphaLibrary</title>
         </Head>
         <body>
           <Main />
