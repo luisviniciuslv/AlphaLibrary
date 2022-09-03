92e2a61f1cb0d0f32455798ed5ee8563ce60ea2e
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2022-09-03 15:22:58 -0300

    Search Component finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import { MangaCard } from '../components/MangaCard'
 
 import styled from "styled-components";
+import { MagnifyingGlass } from 'phosphor-react'
 
 export const MangaCards = styled.div`
   margin-top: 3.125rem;
@@ -19,6 +20,75 @@ export const Title = styled.h1`
   font-size: 2rem;
   font-weight: bold;
   margin-top: 2rem;
+`
+
+export const SearchArea = styled.div`
+  margin-top: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  p {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+`
+
+export const SearchContent = styled.div`
+  display: flex;
+  align-items: center;
+
+  gap: 0.5rem;
+
+  background-color: ${props => props.theme['gray-900']};
+
+  padding: 0.5rem;
+
+  border-radius: 6px;
+`
+
+export const SearchInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.75rem;
+
+  padding: 0.562rem 0.375rem;
+
+  background-color: ${props => props.theme['white-50']};
+  border-radius: 6px;
+
+  svg {
+    color: ${props => props.theme['gray-900']};
+  }
+`
+
+export const SearchInput = styled.input`
+  border: none;
+
+  width: 31.75rem;
+
+  color: ${props => props.theme['gray-800']};
+
+  ::placeholder {
+    color: ${props => props.theme['gray-400']};
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
+  :focus {
+    box-shadow: none;
+  }
+`
+
+export const Select = styled.select`
+  border: none;
+
+  width: 19.125rem;
+  padding: 0.6rem 0.375rem;
+
+  border-radius: 6px;
 `
 
 const Home: NextPage = () => {
@@ -55,7 +125,22 @@ const Home: NextPage = () => {
           destacado
         />
       </MangaCards>
-      <Title>Descobrir</Title>
+      <SearchArea>
+        <p>Descobrir</p>
+        <SearchContent>
+          <SearchInputContainer>
+            <MagnifyingGlass size={18} />
+            <SearchInput placeholder='Pesquisar' />
+          </SearchInputContainer>
+          <Select>
+            <option value=''>Categorias</option>
+            <option value='romance'>Romance</option>
+            <option value='ação'>Ação</option>
+            <option value='militar'>Militar</option>
+            <option value='shounem'>Shounem</option>
+          </Select>
+        </SearchContent>
+      </SearchArea>
       <MangaCards>
         <MangaCard
           id='1'
